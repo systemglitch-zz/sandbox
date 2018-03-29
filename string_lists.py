906ab7695b1b8dcb30bd1405d9483b3d09dd6abc
curtis <csalinas@gmail.com>
2018-03-29 08:19:05 -0700

    commiting working palindrome function

--- string_lists.py
+++ string_lists.py
@@ -1,22 +1,25 @@
 a = [1,2,3,2,1]
-b = [1,2,3,4,3,2,1]
-c = [1,2,3,4,5,3,2,1]
-d = "thisissithis"
-e = "thisisnotthis"
+b = [3]
+c = [1,2,3,4,3,2,1]
+d = [1,2,3,4,5,3,2,1]
+e = "thisissisiht"
+f = "thisisnotthis"
+g = [1,3,1]
 
-x = 0
+def isPalindrome(myList):
+    while len(myList) >= 0:
+        print(len(myList))
+        print(myList)
 
-for x in range(0, (len(a)-1)):
-    newList = a[x:(x*(-1))]
-    print(len(newList))
-    print(newList)
-    if newList[x] == newList[x*(-1)]:
-        x += 1
-    elif len(newList) == 0 or len(newList) == 1:
-        print("your list is a palindrome")
-        break
-    else:
-        print("your list is NOT a palindrome")
-        x = len(a) + 1
+        if len(myList) <= 1:
+            print("your list is a palindrome")
+            break
+        
+        elif myList[0] != (myList[len(myList)-1]):
+            print("your list is NOT a palindrome")
+            break
+
+        else:
+            myList = myList[1:(len(myList)-1)]
 
 
